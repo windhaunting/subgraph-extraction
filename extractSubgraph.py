6b6b0d02f24b3b53696c4f81bea075eea8fbcfa0
fubao <freadream@gmail.com>
2017-09-21 09:37:01 -0400

    update_function

--- extractSubgraph.py
+++ extractSubgraph.py
@@ -71,8 +71,8 @@ class ClsSubgraphExtraction(object):
                     #get all path
                     #print(" xxxx dddddd", src, dst)
                     print ("nx.all_simple_paths: ")
-                    print (" ", list(nx.all_simple_paths(G, src, dst)))
-                    for path in nx.all_simple_paths(G, src, dst):
+                    #print (" ", list(nx.all_simple_paths(G, src, dst, cutoff= 100)))
+                    for path in nx.all_simple_paths(G, src, dst, cutoff= 10):
                         #check how many product inside the path
                         #check how many has product type in the path
                         #print(" path aaaaa", len(path))
